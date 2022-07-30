ce8695779c4f185bd99cf9c1db2d62f2dd5af900
Yang Liu <yangliu3456@gmail.com>
2022-07-30 16:42:29 -0400

    moved play.py and readme file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mahjong
 Playing the game of Mahjong
+
+# Start
+`play.py` to start playing the game
+
+# Conventions
+w - 万
+t - 条
+b - 饼
+f - 风（按'东南西北中发白'的顺序）
+h - 花牌 (按'春夏秋冬梅兰竹菊'的顺序)
+For example, 1w is 一万
--- mahjong/suit.py
+++ mahjong/suit.py
@@ -9,13 +9,12 @@ class SuitEnum():
 
 class FengSuitEnum():
     DONG = 1
-    XI = 2
-    NAN = 3
+    NAN = 2
+    XI = 3
     BEI = 4
     ZHONG = 5
     FA = 6
-    CAI = 7
-    BAN = 8
+    BAI = 7
 
 @dataclass
 class Suit:
