8ab769ceb24ce31ff23127e96b72febcef5975d3
Yang Liu <yangliu3456@gmail.com>
2022-10-27 19:50:18 -0400

    use autoapi to add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = ['sphinx.ext.duration',
               'sphinx.ext.doctest',
               'sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
+              'autoapi.extension'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,3 +57,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# setup auto api
+autoapi_dirs = ['../mahjong']
